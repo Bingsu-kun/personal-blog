9d00123ba32e66381d165dd189e3c5bdf7799f8a
Bingsu-kun <icetime963@gmail.com>
2023-10-19 16:26:56 +0900

    Feat: convert js to ts with vite

---
+++ Front/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div id="App-header"></div>
+      <div id="App-body"></div>
+      <div id="App-footer"></div>
+    </>
+  );
+}
+
+export default App;
