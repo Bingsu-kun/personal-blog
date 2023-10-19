ca3d0ebaefd546044759e70b6e0d8b137d7c9d96
Bingsu-kun <icetime963@gmail.com>
2023-10-19 17:28:34 +0900

    Feat: nav bar

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Navbar from "./components/nav/navbar";
 
 function App() {
   return (
     <>
-      <div id="App-header"></div>
+      <div id="App-header">
+        <Navbar />
+      </div>
       <div id="App-body"></div>
       <div id="App-footer"></div>
     </>
