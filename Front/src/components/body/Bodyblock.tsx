07a1fc6b6da8e8e0e752f70e8a1d7f4a01438e5a
Bingsu-kun <icetime963@gmail.com>
2023-10-26 17:47:27 +0900

    Fix: make Bodyblock to take contents

--- Front/src/components/body/Bodyblock.tsx
+++ Front/src/components/body/Bodyblock.tsx
@@ -1,6 +1,11 @@
 import { motion } from "framer-motion";
+import { ReactElement } from "react";
 
-function Block(props: { blockId: string; isHover?: boolean }) {
+function Block(props: {
+  blockId: string;
+  isHover?: boolean;
+  contents?: ReactElement;
+}) {
   const id: string = "block-" + `${props.blockId}`;
 
   return props?.isHover ? (
@@ -9,12 +14,16 @@ function Block(props: { blockId: string; isHover?: boolean }) {
       whileHover={{ scale: 1.03 }}
       transition={{ type: "spring", stiffness: 200, damping: 10 }}
       className="shadow-xl rounded-xl bg-[var(--dark-block-bg)] justify-center items-center border border-white border-opacity-[.08] my-10 px-10 py-5 hover:bg-[#384264]"
-    ></motion.div>
+    >
+      {props.contents}
+    </motion.div>
   ) : (
     <div
       id={id}
       className="shadow-xl rounded-xl bg-[var(--dark-block-bg)] justify-center items-center border border-white border-opacity-[.08] my-10 px-10 py-5"
-    ></div>
+    >
+      {props.contents}
+    </div>
   );
 }
 
