e25c3751991679d1ff486329230d8c384ab6f194
Bingsu-kun <icetime963@gmail.com>
2023-10-26 16:27:30 +0900

    FIx: Bodyblock component
    - add isHover prop for no hover animation

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <div id="App-body" className="grid grid-cols-[20%_60%_20%]">
         <div id="App-body-left"></div>
         <div id="App-body-center">
-          <Block blockId="1" />
+          <Block blockId="1" isHover={true} />
           <Block blockId="2" />
         </div>
         <div id="App-body-right"></div>
--- Front/src/components/body/Bodyblock.tsx
+++ Front/src/components/body/Bodyblock.tsx
@@ -1,15 +1,20 @@
 import { motion } from "framer-motion";
 
-function Block(props: { blockId: string }) {
-  const id = "block-" + `${props.blockId}`;
+function Block(props: { blockId: string; isHover?: boolean }) {
+  const id: string = "block-" + `${props.blockId}`;
 
-  return (
+  return props?.isHover ? (
     <motion.div
       id={id}
       whileHover={{ scale: 1.03 }}
       transition={{ type: "spring", stiffness: 200, damping: 10 }}
       className="shadow-xl rounded-xl bg-[var(--dark-block-bg)] justify-center items-center border border-white border-opacity-[.08] my-10 px-10 py-5 hover:bg-[#384264]"
     ></motion.div>
+  ) : (
+    <div
+      id={id}
+      className="shadow-xl rounded-xl bg-[var(--dark-block-bg)] justify-center items-center border border-white border-opacity-[.08] my-10 px-10 py-5"
+    ></div>
   );
 }
 
