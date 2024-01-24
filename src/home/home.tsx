7ba35489b4ff3ca4f1c474593c7999c4bb7f6fec
bingsu-kun <icetime963@gmail.com>
2024-01-24 16:45:01 +0900

    Feat: Swiper 적용 (not tested)

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -4,11 +4,11 @@ import Intro from "./intro";
 const Home = () => {
   return (
     <>
-      <div id="Home-body" className="grid grid-cols-[30%_70%]">
+      <div id="Home-body" className="flex">
         <div id="Home-body-profile">
             <Profile/>
         </div>
-        <div id="Home-body-intro">
+        <div id="Home-body-intro" className="flex justify-center items-center w-full">
             <Intro/>
         </div>
       </div>
--- src/home/intro.tsx
+++ src/home/intro.tsx
@@ -1,6 +1,32 @@
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Mousewheel, Pagination } from 'swiper/modules';
+
 const Intro = () => {
-    return <></>
+    return (
+        <>
+            <Swiper
+                direction={'vertical'}
+                slidesPerView={1}
+                spaceBetween={30}
+                mousewheel={true}
+                pagination={{
+                clickable: true,
+                }}
+                modules={[Mousewheel, Pagination]}
+                className="w-full h-full"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+        </>
+    )
 }
 
 export default Intro
